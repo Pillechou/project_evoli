19b4766d05a712509ae9f473ff062e72f5bbbca9
Pillechou <laurasnap62@gmail.com>
2022-10-13 07:49:24 +0200

    Delete content of preload

--- prelaod.js
+++
@@ -1,9 +0,0 @@
-const electron = require('electron')
-const ipc = electron.ipcRenderer
-
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('discordbtn').addEventListener("click", function () {
-        let active_hotspot_id = localStorage.getItem('active_hotspot_id')
-        const reply = ipc.sendSync('hotspot-event', active_hotspot_id)
-    });
-})
